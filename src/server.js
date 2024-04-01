c931fa415366be0c74caec0bc625ef8d210d5773
k1v4z <addnguyen32gmail.com>
2024-04-01 21:19:18 +0700

    inital

---
+++ src/server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const express = require('express');
+//const router = require('./route/web');
+//const setViewEngine = require('./config/ViewEngine');
+////const bodyParser = require('body-parser');
+
+//const cookieParser = require('cookie-parser');
+const app = express()
+const port = process.env.PORT;
+const localhost = process.env.HOST;
+
+//config cookie parser
+//app.use(cookieParser());
+
+//config view engine
+//setViewEngine(app);
+//initApiRoute(app);
+
+//config req.body
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+//app.use(router);
+// app.use((req, res) => {
+//     res.send('404 NOT Found');
+// })
+
+
+app.get('/', (req, res) => {
+    res.send('hello');
+})
+app.listen(port, localhost, () => {
+    console.log(`http://${localhost}:${port}`);
+})
