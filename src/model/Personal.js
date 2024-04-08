2a61c59e77bba42e5789a98532e503e17213e5e3
k1v4z <addnguyen32gmail.com>
2024-04-08 23:51:53 +0700

    create model 08/04/2024

---
+++ src/model/Personal.js
@@ -0,0 +1,29 @@
+const { sequelize_sqlserver } = require('../config/Sequelize');
+const {DataTypes} = require('sequelize');
+
+const Personal = sequelize_sqlserver.define('Personal',{
+    Employee_ID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+
+    First_Name: DataTypes.STRING,
+    Middle_Initial: DataTypes.STRING,
+    Address1: DataTypes.STRING,
+    Address2: DataTypes.STRING,
+    City: DataTypes.STRING,
+    State: DataTypes.STRING,
+    Zip: DataTypes.NUMBER,
+    Email: DataTypes.STRING,
+    Phone_Number: DataTypes.STRING,
+    Social_Security_Number: DataTypes.STRING,
+    Drivers_License: DataTypes.STRING,
+    Marital_Status: DataTypes.STRING,
+    Gender: DataTypes.BOOLEAN,
+    Shareholder_Status: DataTypes.BOOLEAN,
+    Benefit_Plans: DataTypes.INTEGER,
+    Ethnicity: DataTypes.STRING
+
+});
+
+module.exports = Personal
