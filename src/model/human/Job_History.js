85337c93e2f2bb7cd64c4522a5d0260453e3b245
k1v4z <addnguyen32gmail.com>
2024-04-10 13:16:13 +0700

    define association

--- src/model/human/Job_History.js
+++ src/model/human/Job_History.js
@@ -1,5 +1,7 @@
 const { sequelize_sqlserver } = require('../../config/Sequelize');
 const { DataTypes } = require('sequelize');
+const Personal = require('./Personal');
+const Employment = require('./Employment');
 
 const Job_History = sequelize_sqlserver.define('Job_History',{
     ID: {
@@ -20,6 +22,13 @@ const Job_History = sequelize_sqlserver.define('Job_History',{
     Pay_Period: DataTypes.STRING,
     Hours_per_Week: DataTypes.INTEGER,
     Hazardous_Training: DataTypes.BOOLEAN
-}, { sequelize_sqlserver, modelName: 'Job_History', tableName: 'Job_History'})
+}, { sequelize_sqlserver, modelName: 'Job_History', tableName: 'Job_History'});
+
+//define association
+Personal.hasMany(Job_History,{foreignKey: 'Employee_ID'});
+Job_History.belongsTo(Personal,{foreignKey: 'Employee_ID'});
+
+Personal.hasOne(Employment,{foreignKey: 'Employee_ID'});
+Employment.belongsTo(Personal,{foreignKey: 'Employee_ID'});
 
 module.exports = Job_History
