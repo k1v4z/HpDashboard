02d30d8a9b17c1b6f9412c9d6ea51b92ef39f6ea
k1v4z <addnguyen32gmail.com>
2024-04-11 09:15:45 +0700

    calculate total benefit 11/04/2024

--- src/service/TotalBenefits.service.js
+++ src/service/TotalBenefits.service.js
@@ -1,10 +1,8 @@
 const { sequelize_mysql } = require("../config/Sequelize");
-const { checkPersonalWhere, checkEmploymentWhere } = require("../helper/CheckCondidtion.helper");
+const { checkPersonalWhere, checkEmploymentWhere } = require("../helper/CheckCondition.helper");
 const Employment = require("../model/human/Employment");
 const Job_History = require("../model/human/Job_History");
 const Personal = require("../model/human/Personal");
-const Employee = require("../model/payroll/Employees");
-const PayRate = require("../model/payroll/Pay_rates");
 const { QueryTypes } = require('sequelize');
 
 const CalculateTotalBenefits = async () => {
@@ -15,7 +13,9 @@ const CalculateTotalBenefits = async () => {
     const choice = 'Employment_Status'; //Gender or Ethinicity or Shareholder status option 3
     const choiceValue = 'Part-Time';
 
-    const personalAttribute = await Job_History.findAll({
+    let Total_Benefit = 0;
+
+    const humans = await Job_History.findAll({
         attributes: ['Employee_ID', 'Department'],
         where: { Department: department },
         include: [{
@@ -33,7 +33,7 @@ const CalculateTotalBenefits = async () => {
         .catch(err => console.log(err));
 
     //raw query
-    const payrollAttribute = await sequelize_mysql.query(
+    const payroll = await sequelize_mysql.query(
         `SELECT e.\`${choice_year}\`, p.\`Tax Percentage\`, p.\`Pay Amount\`, e.\`idEmployee\`
         FROM mydb.employee AS e
         INNER JOIN mydb.\`pay rates\` AS p ON p.\`idPay Rates\` = e.\`Pay Rates_idPay Rates\`;`,
@@ -42,8 +42,16 @@ const CalculateTotalBenefits = async () => {
         .then(StringJSON => JSON.parse(StringJSON))
         .catch(err => console.log(err));
 
-    console.log(personalAttribute);
-    console.log(payrollAttribute[0]);
+    const benefit = humans.map((human, index) => {
+        human.fullname = human.Personal.First_Name + ' ' + human.Personal.Middle_Initial + ' ' + human.Personal.Last_Name;
+        const tempAmount = payroll[index]['Pay Amount'] * payroll[index][`${choice_year}`];
+        human.Total_benefit = tempAmount - (tempAmount * payroll[index]['Tax Percentage'] / 100);
+        Total_Benefit += human.Total_benefit;
+        return human;
+    });
+
+    console.log(Total_Benefit);
+    console.log(benefit);
 }
 
 
