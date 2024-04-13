dd47466dfc15ac9f7c165da43fcf24d1e20f5f94
k1v4z <addnguyen32gmail.com>
2024-04-13 10:53:30 +0700

    define association between benefit plan and personal 13/04/2024

--- src/model/human/Benefit_Plans.js
+++ src/model/human/Benefit_Plans.js
@@ -1,5 +1,7 @@
 const { sequelize_sqlserver } = require('../../config/Sequelize');
 const { DataTypes } = require('sequelize');
+const Personal = require('./Personal');
+
 
 const Benefit_Plans = sequelize_sqlserver.define('BENEFIT_PLANS', {
     BENEFIT_PLANS_ID: {
@@ -10,5 +12,8 @@ const Benefit_Plans = sequelize_sqlserver.define('BENEFIT_PLANS', {
     DEDUCTABLE: DataTypes.INTEGER,
     PERCENTAGE_COPAY: DataTypes.INTEGER
 }, { sequelize_sqlserver, modelName: 'BENEFIT_PLANS', tableName: 'BENEFIT_PLANS' });
+
+Benefit_Plans.hasMany(Personal, { foreignKey: 'BENEFIT_PLAN_ID' });
+Personal.belongsTo(Benefit_Plans, { foreignKey: 'BENEFIT_PLAN_ID' });
 
 module.exports = Benefit_Plans
