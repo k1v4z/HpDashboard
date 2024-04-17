27ff322d25d9ab9ec3a1234ba7f5257da49ba833
k1v4z <addnguyen32gmail.com>
2024-04-17 09:30:10 +0700

    add api total earning 17/04/2024

---
+++ src/route/api/API_Total_Earning.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { getTotalEarning } = require('../../controller/ApiController');
+
+let router = express.Router();
+
+const init_API_Total_Earning = (app) => {
+    router.get('/total-earnings', getTotalEarning);
+    app.use('/api/v1/', router);
+}
+
+module.exports = init_API_Total_Earning
