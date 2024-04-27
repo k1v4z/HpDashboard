7f89982ba0b085d8cee0c837c7349441e20f906a
k1v4z <addnguyen32gmail.com>
2024-04-27 13:40:49 +0700

    update em route 28/04/2024

--- src/route/web/em.js
+++ src/route/web/em.js
@@ -1,16 +1,11 @@
 //em stand for employee management
-//this file will define endpoint of employment route
+//This file will define endpoint of employment route
 
 const express = require('express');
-const { getListEmployee } = require('../../service/Dashboard.service');
+const { getAllEmployee } = require('../../controller/EmController');
 const routerManage = express.Router();
 
-routerManage.get('/manage', async (req, res) => {
-    const listEmployee = await getListEmployee();
-    return res.render('employee_management.ejs', {
-        listEmployee: listEmployee
-    });
-});
+routerManage.get('/manage', getAllEmployee);
 
 module.exports = routerManage;
 //temporary for FE test UI
