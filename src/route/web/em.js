65a5e101cc0a3962491b5c75f26f7405575a496f
k1v4z <addnguyen32gmail.com>
2024-05-09 10:53:32 +0700

    add middle valid to route

--- src/route/web/em.js
+++ src/route/web/em.js
@@ -3,13 +3,15 @@
 
 const express = require('express');
 const { getAllEmployee, addEPI, DeletePersonalView, getEmployeeView, getEmployeeAdd, postUpdateEmploymentPage, setEditDataToFormEmploymentEdit, setEditDataToFormPersonalEdit, postInsertOrUpdatePersonalPage } = require('../../controller/EmController');
+const checkBenefitPlanId = require('../../helper/Valid_IdBenefitPlan.helper');
+const valid = require('../../middleware/Valid_Id');
 
 const routerManage = express.Router();
 
 routerManage.get('/manage', getAllEmployee);
 
 //CRUD Action
-routerManage.post('/add', addEPI)
+routerManage.post('/add', [valid.validBenefitPlanId, valid.validIdPayRate], addEPI)
 routerManage.get('/employee-view', getEmployeeView);
 routerManage.delete('/employee-view/:id', DeletePersonalView);
 
