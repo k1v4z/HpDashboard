a17638d8bac07a6754d9937d3463b263c8abf4e5
k1v4z <addnguyen32gmail.com>
2024-05-15 09:30:26 +0700

    update controller

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -1,3 +1,6 @@
+const Group = require("../model/user/Group")
+const Module = require("../model/user/Module")
+const { User } = require("../model/user/User")
 const { auth, authSignUp, authLogin } = require("../service/Authentication.service")
 
 const getFormLogin = (req, res) => {
@@ -17,10 +20,21 @@ const signUpAuth = async (req, res) => {
 
 const loginAuth = async (req, res) => {
     const { username, password } = req.query
-    const status = await authLogin(username, password)
+    const user = new User(null,username,password)
+    const status = await authLogin(user) //get status fail or success
+    const groupOfUser = await user.getGroup() //get group
+    const group = new Group(null,null)
+    const moduleOfGroup = await group.getModule(groupOfUser)
+    const module = new Module(null,null)
+    const func = await user.getFunction()
 
-    if (status == 'Login success') {
+    if (status) {
         req.session.user = username
+        req.session.userid = status
+        req.session.group = groupOfUser
+        req.session.module = moduleOfGroup
+        req.session.function = func
+
         return res.redirect('/')
     } else {
         res.send('Username or password is incorrect')
