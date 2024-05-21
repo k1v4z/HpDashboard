3c28c60da2d3774490fd450f70852caa6bbbbe0b
k1v4z <addnguyen32gmail.com>
2024-05-21 23:00:36 +0700

    add addGroup func

--- src/controller/ApiAuthorization.js
+++ src/controller/ApiAuthorization.js
@@ -1,3 +1,21 @@
-const addGroup = (req, res) => {
-    
+const Group = require("../model/user/Group")
+const { addGroupService } = require("../service/Authorization.service")
+
+const addGroup = async (req, res) => {
+    const groupName = req.query.groupName
+    const status = await addGroupService(groupName)
+
+    if (status == 1) {
+        return res.status(200).json({
+            message: 'Add group successful'
+        })
+    } else {
+        return res.status(500).json({
+            message: 'Add group fail'
+        })
+    }
+}
+
+module.exports = {
+    addGroup
 }
