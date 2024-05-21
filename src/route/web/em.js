0f0e7d566b80c84bfbc2932a0fa99e97973bafae
k1v4z <addnguyen32gmail.com>
2024-05-21 20:47:39 +0700

    update 21/04/2024

--- src/route/web/em.js
+++ src/route/web/em.js
@@ -2,7 +2,7 @@
 //This file will define endpoint of employment route
 
 const express = require('express');
-const { getEmployeePm,DeleteEmployedelete,getAllEmployee, addEPI, DeletePersonalView, getEmployeeView, getEmployeeAdd, getChangeBenefitPlan, postUpdateEmploymentPage, setEditDataToFormEmploymentEdit, setEditDataToFormPersonalEdit, postInsertOrUpdatePersonalPage } = require('../../controller/EmController');
+const { getEmployeePm, DeleteEmployedelete, getAllEmployee, addEPI, DeletePersonalView, getEmployeeView, getEmployeeAdd, getChangeBenefitPlan, postUpdateEmploymentPage, setEditDataToFormEmploymentEdit, setEditDataToFormPersonalEdit, postInsertOrUpdatePersonalPage } = require('../../controller/EmController');
 const valid = require('../../middleware/Valid_Id');
 const requireFunctionPermission = require('../../middleware/function_middleware');
 
@@ -12,19 +12,19 @@ routerManage.get('/manage', getAllEmployee);
 
 //CRUD Action
 routerManage.post('/add', [valid.validBenefitPlanId, valid.validIdPayRate], addEPI)
-routerManage.get('/employee-add',requireFunctionPermission,getEmployeeAdd);
+routerManage.get('/employee-add', requireFunctionPermission, getEmployeeAdd);
 
 routerManage.get('/employee-view', getEmployeeView);
 routerManage.get('/employee-ManagerView', getEmployeePm);
 
-routerManage.get('/personal/edit/:id', requireFunctionPermission,setEditDataToFormPersonalEdit);
+routerManage.get('/personal/edit/:id', requireFunctionPermission, setEditDataToFormPersonalEdit);
 routerManage.post('/update', postInsertOrUpdatePersonalPage)
-routerManage.get('/employment/edit/:id',requireFunctionPermission,setEditDataToFormEmploymentEdit);
+routerManage.get('/employment/edit/:id', requireFunctionPermission, setEditDataToFormEmploymentEdit);
 routerManage.post('/update', postUpdateEmploymentPage)
-routerManage.get('/benefit_plan/:id',getChangeBenefitPlan)
+routerManage.get('/benefit_plan/:id', getChangeBenefitPlan)
 
 routerManage.delete('/personal-view/:id', DeletePersonalView);
-routerManage.delete('/employee-ManagerView/:id',DeleteEmployedelete);
+routerManage.delete('/employee-ManagerView/:id', DeleteEmployedelete);
 
 module.exports = routerManage;
 //temporary for FE test UIz
