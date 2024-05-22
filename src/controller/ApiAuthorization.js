f1d6df108356f9bed2f3f3e6020c84dfe4cb4b69
k1v4z <addnguyen32gmail.com>
2024-05-22 08:24:37 +0700

    add module, add function, get all module

--- src/controller/ApiAuthorization.js
+++ src/controller/ApiAuthorization.js
@@ -1,5 +1,5 @@
 const Group = require("../model/user/Group")
-const { addGroupService } = require("../service/Authorization.service")
+const { addGroupService, addModuleService, getAllModule, addFunctionService } = require("../service/Authorization.service")
 
 const addGroup = async (req, res) => {
     const groupName = req.query.groupName
@@ -16,6 +16,54 @@ const addGroup = async (req, res) => {
     }
 }
 
+const addModule = async (req, res) => {
+
+    const moduleName = req.query.moduleName
+    const status = await addModuleService(moduleName)
+
+    if (status == 1) {
+        return res.status(200).json({
+            message: 'Add group successful'
+        })
+    } else {
+        return res.status(500).json({
+            message: 'Add group fail'
+        })
+    }
+}
+
+const addFunction = async (req, res) => {
+    const { functionName, moduleId } = req.query
+    const status = await addFunctionService(functionName,moduleId)
+
+    if (status == 1) {
+        return res.status(200).json({
+            message: 'Add group successful'
+        })
+    } else {
+        return res.status(500).json({
+            message: 'Add group fail'
+        })
+    }
+}
+
+const getAllModules = async (req, res) => {
+    const modules = await getAllModule()
+
+    try{
+        return res.status(200).json({
+            modules: modules
+        })
+    }catch(err){
+        return res.status(500).json({
+            message: err
+        })
+    }
+}
+
 module.exports = {
-    addGroup
+    addGroup,
+    addModule,
+    addFunction,
+    getAllModules
 }
