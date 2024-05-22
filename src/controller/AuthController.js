de77a69d1031e94ab28f99445e6c828b150a27c4
tandung10 <nguyentandung20031011@gmail.com>
2024-05-22 23:01:43 +0700

    Style UI

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -3,7 +3,7 @@ const Module = require("../model/user/Module")
 const { User } = require("../model/user/User")
 const md5 = require('md5');
 const { auth, authSignUp, authLogin } = require("../service/Authentication.service")
-const pwd = null;
+let pwd = null;
 
 const getFormLogin = (req, res) => {
     return res.render('login.ejs')
@@ -24,15 +24,15 @@ const signUpAuth = async (req, res) => {
 const loginAuth = async (req, res) => {
     const { username, password } = req.query
     const loginPwd = md5(password);
-    const user = new User(null,username, loginPwd)
+    const user = new User(null, username, loginPwd)
     const status = await authLogin(user) //get status fail or success
     const groupOfUser = await user.getGroup() //get group
-    const group = new Group(null,null)
+    const group = new Group(null, null)
     const moduleOfGroup = await group.getModule(groupOfUser)
-    const module = new Module(null,null)
+    const module = new Module(null, null)
     const func = await user.getFunction()
     console.log(func)
-    
+
     if (status) {
         req.session.user = username
         req.session.userid = status
