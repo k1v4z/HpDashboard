9ae4fcb6d16f64ec242e448149b7fd5270ad40bb
k1v4z <addnguyen32gmail.com>
2024-05-22 08:14:09 +0700

    add module service

--- src/service/Authorization.service.js
+++ src/service/Authorization.service.js
@@ -1,12 +1,41 @@
+const Function = require("../model/user/Function")
 const Group = require("../model/user/Group")
+const Module = require("../model/user/Module")
 
-const addGroupService = async(groupName)=>{ //add service to avoid duplicate name with controller
+const addGroupService = async (groupName) => { //add service to avoid duplicate name with controller
     const group = new Group(null, groupName)
     const addGroupStatus = await group.addGroup()
 
     return addGroupStatus
 }
 
+const addModuleService = async (moduleName) => {
+    const module = new Module(null, moduleName)
+    const addModuleStatus = await module.addModule()
+
+    return addModuleStatus
+}
+
+const getAllModule = async () => {
+    const module = new Module(null, null)
+
+    const modules = await module.findAll()
+
+    return modules
+}
+
+const addFunctionService = async (functionName,moduleId) => {
+    const func = new Function(null,functionName,moduleId)
+
+    const addFunctionStatus = await func.addFunction()
+
+    return addFunctionStatus
+}
+
+
 module.exports = {
-    addGroupService
+    addGroupService,
+    addModuleService,
+    getAllModule,
+    addFunctionService
 }
