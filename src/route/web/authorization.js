78279b1fc219a6e0e333e73c0e3ea21c93ed9b4d
k1v4z <addnguyen32gmail.com>
2024-05-22 08:15:13 +0700

    add modules,functions route, and findAll

--- src/route/web/authorization.js
+++ src/route/web/authorization.js
@@ -1,11 +1,15 @@
 const express = require('express')
-const { addGroup } = require('../../controller/ApiAuthorization')
+const { addGroup, addModule, addFunction, getAllModules } = require('../../controller/ApiAuthorization')
 const { requireLogin } = require('../../middleware/login_middleware')
 const requireFunctionPermission = require('../../middleware/function_middleware')
 const routerAuth = express.Router()
 
 const initAuthorization = (app) => {
     routerAuth.post('/groups/add', addGroup)
+    routerAuth.post('/modules/add', addModule)
+    routerAuth.post('/functions/add', addFunction)
+    routerAuth.get('/modules/findAll',getAllModules)
+    
     app.use('/api/v1/',routerAuth)
 }
 
