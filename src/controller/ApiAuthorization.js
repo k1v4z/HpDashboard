0abb8504ffb06edb49e5d4c992489abf5595d0f5
k1v4z <addnguyen32gmail.com>
2024-05-22 17:06:26 +0700

    add GET method

--- src/controller/ApiAuthorization.js
+++ src/controller/ApiAuthorization.js
@@ -1,5 +1,5 @@
 const Group = require("../model/user/Group")
-const { addGroupService, addModuleService, getAllModule, addFunctionService } = require("../service/Authorization.service")
+const { addGroupService, addModuleService, getAllModule, addFunctionService, getAllUserService, getAllGroupService, getGroupByUserIdService, getFunctionByUserIdService, getAllFunctionService } = require("../service/Authorization.service")
 
 const addGroup = async (req, res) => {
     const groupName = req.query.groupName
@@ -34,7 +34,7 @@ const addModule = async (req, res) => {
 
 const addFunction = async (req, res) => {
     const { functionName, moduleId } = req.query
-    const status = await addFunctionService(functionName,moduleId)
+    const status = await addFunctionService(functionName, moduleId)
 
     if (status == 1) {
         return res.status(200).json({
@@ -50,20 +50,99 @@ const addFunction = async (req, res) => {
 const getAllModules = async (req, res) => {
     const modules = await getAllModule()
 
-    try{
+    try {
         return res.status(200).json({
             modules: modules
         })
-    }catch(err){
+    } catch (err) {
         return res.status(500).json({
             message: err
         })
     }
 }
 
+const getAllUser = async (req, res) => {
+    const users = await getAllUserService()
+
+    try {
+        return res.status(200).json({
+            usersArray: users
+        })
+    } catch (err) {
+        return res.status(500).json({
+            message: err
+        })
+    }
+}
+
+const getAllGroup = async (req, res) => {
+    const groups = await getAllGroupService()
+
+    try {
+        return res.status(200).json({
+            groupsArray: groups
+        })
+    } catch (err) {
+        return res.status(500).json({
+            message: err
+        })
+    }
+}
+
+const getAllFunction = async (req, res) => {
+    const funcs = await getAllFunctionService()
+
+    try {
+        return res.status(200).json({
+            funcsArray: funcs
+        })
+    } catch (err) {
+        return res.status(500).json({
+            message: err
+        })
+    }
+}
+
+const getGroupByUserId = async (req, res) => {
+    const userId = req.query.userId
+    const groups = await getGroupByUserIdService(userId)
+
+    try {
+        return res.status(200).json({
+            groupsArray: groups
+        })
+    } catch (err) {
+        return res.status(500).json({
+            message: err
+        })
+    }
+}
+
+const getFunctionByUserId = async (req, res) => {
+    const userId = req.query.userId
+    const functions = await getFunctionByUserIdService(userId)
+
+    try {
+        return res.status(200).json({
+            functionsArray: functions
+        })
+    } catch (err) {
+        return res.status(500).json({
+            message: err
+        })
+    }
+}
+
+
+
 module.exports = {
     addGroup,
     addModule,
     addFunction,
-    getAllModules
+    getAllModules,
+    getAllUser,
+    getAllGroup,
+    getAllFunction,
+    getGroupByUserId,
+    getFunctionByUserId
 }
