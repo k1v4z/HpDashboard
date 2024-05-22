7916840cd0d19faed9ce1eba095b59913b551326
k1v4z <addnguyen32gmail.com>
2024-05-22 17:09:07 +0700

    update authorization service

--- src/service/Authorization.service.js
+++ src/service/Authorization.service.js
@@ -1,6 +1,7 @@
 const Function = require("../model/user/Function")
 const Group = require("../model/user/Group")
 const Module = require("../model/user/Module")
+const { User } = require("../model/user/User")
 
 const addGroupService = async (groupName) => { //add service to avoid duplicate name with controller
     const group = new Group(null, groupName)
@@ -24,12 +25,57 @@ const getAllModule = async () => {
     return modules
 }
 
-const addFunctionService = async (functionName,moduleId) => {
-    const func = new Function(null,functionName,moduleId)
+const addFunctionService = async (functionName, moduleId) => {
+    const func = new Function(null, functionName, moduleId)
 
     const addFunctionStatus = await func.addFunction()
 
     return addFunctionStatus
+}
+
+const getAllUserService = async () => {
+    const user = new User(null, null, null)
+    const users = await user.findAll()
+
+    return users
+}
+
+const getAllGroupService = async () => {
+    const group = new Group(null, null)
+    const groups = await group.findAll()
+
+    return groups
+}
+
+const getAllFunctionService = async () => {
+    const func = new Function(null, null)
+    const funcs = await func.findAll()
+
+    return funcs
+}
+
+const getGroupByUserIdService = async (userId) => {
+    const user = new User(userId, null, null)
+
+    const groups = await user.getGroup()
+
+    return groups
+}
+
+const getFunctionByUserIdService = async (userId) => {
+    const user = new User(userId, null, null)
+
+    const functions = await user.getFunction()
+
+    return functions
+}
+
+const getGroupFollowModuleService = async (moduleId) => {
+    const module = new Module(moduleId,null)
+
+    const modules = await module.getGroupFollowModule()
+
+    return modules
 }
 
 
@@ -37,5 +83,11 @@ module.exports = {
     addGroupService,
     addModuleService,
     getAllModule,
-    addFunctionService
+    addFunctionService,
+    getAllUserService,
+    getAllGroupService,
+    getGroupByUserIdService,
+    getFunctionByUserIdService,
+    getAllFunctionService,
+    getGroupFollowModuleService
 }
