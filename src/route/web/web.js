965f8c2d8657da2ec5a62e27841c6048f4559e10
k1v4z <addnguyen32gmail.com>
2024-05-22 17:08:41 +0700

    add authorize endpoint

--- src/route/web/web.js
+++ src/route/web/web.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const { getEmployeeViewEdit, getEmployeeAdd, getEmployeeView,
     getDashBoard, getTotalEarnings, getVacationDays,
     getAverageBenefitPaid, getAddAccessControl, getAnnouncementOne, getAnnouncementTwo, getAccessControl,
-    getManagementForm } = require('../../controller/HomeController');
+    getManagementForm,
+    getAuthorization } = require('../../controller/HomeController');
 const { requireLogin } = require('../../middleware/login_middleware');
 const requireModulePermission = require('../../middleware/module_middleware');
 
@@ -16,5 +17,6 @@ router.get('/detail_announcement_2', getAnnouncementTwo);
 router.get('/access_control', requireLogin, requireModulePermission, getAccessControl);
 router.get('/add_authorization', requireLogin, requireModulePermission, getAddAccessControl);
 router.get('/management', requireLogin, requireModulePermission, getManagementForm)
+router.get('/authorize', requireLogin, getAuthorization)
 
 module.exports = router;
