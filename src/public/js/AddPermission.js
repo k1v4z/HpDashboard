0e22e8a8763eaa69646ea75dab93090977048014
k1v4z <addnguyen32gmail.com>
2024-05-22 08:22:56 +0700

    handle add function, group, module

--- src/public/js/AddPermission.js
+++ src/public/js/AddPermission.js
@@ -1,8 +1,37 @@
 const btnAdd = document.querySelector('.btn-add')
 
+const getAllModule = async () => {
+    const modules = await fetch(`http://localhost:4080/api/v1/modules/findAll`)
+        .then(res => res.json())
+        .catch(err => console.log(err))
+
+    return modules
+}
+
 const addGroup = async () => {
     const inputValue = document.querySelector('.enter-group').value
-    await fetch(`http://localhost:4080/api/v1/groups/add?groupName=${inputValue}`,{
+    await fetch(`http://localhost:4080/api/v1/groups/add?groupName=${inputValue}`, {
+        method: 'POST'
+    })
+        .then(res => alert("Successful"))
+        .catch(err => console.log(err))
+}
+
+const addFunction = async () => {
+    const inputValue = document.querySelector('.enter-function').value
+    const moduleValue = document.getElementById('module-function').value
+
+    await fetch(`http://localhost:4080/api/v1/functions/add?functionName=${inputValue}&moduleId=${moduleValue}`, {
+        method: 'POST'
+    })
+        .then(res => alert("Successful"))
+        .catch(err => console.log(err))
+}
+
+
+const addModule = async () => {
+    const inputValue = document.querySelector('.enter-module').value
+    await fetch(`http://localhost:4080/api/v1/modules/add?moduleName=${inputValue}`, {
         method: 'POST'
     })
         .then(res => alert("Successful"))
@@ -13,7 +42,7 @@ const add = () => {
     const selectedItem = document.getElementById("dropdown-select").value
 
     if (selectedItem == 'function') {
-
+        addFunction()
     }
 
     if (selectedItem == 'group') {
@@ -21,9 +50,25 @@ const add = () => {
     }
 
     if (selectedItem == 'module') {
-
+        addModule()
     }
 }
+
+const loadModuleIntoView = async () => {
+    const module_function = document.getElementById('module-function')
+    let moduleFunctionHTML = ''
+
+    if (document.getElementById("dropdown-select").value == 'function') {
+        const modules = await getAllModule()
+        modules.modules.forEach((module) => {
+            moduleFunctionHTML += `<option value=${module.MODULE_ID}>${module.MODULE_NAME}</option>`
+        })
+
+        module_function.innerHTML = moduleFunctionHTML
+    }
+}
+
+loadModuleIntoView()
 
 btnAdd.addEventListener('click', () => {
     add()
