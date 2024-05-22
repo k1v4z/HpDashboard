916ba448b7f0d1610f87102f7e91137d9df8d8a3
k1v4z <addnguyen32gmail.com>
2024-05-22 17:08:03 +0700

    add find route

--- src/route/web/authorization.js
+++ src/route/web/authorization.js
@@ -1,16 +1,23 @@
 const express = require('express')
-const { addGroup, addModule, addFunction, getAllModules } = require('../../controller/ApiAuthorization')
+const { addGroup, addModule, addFunction, getAllModules, getAllUser, getAllGroup, getGroupByUserId, getFunctionByUserId, getAllFunction } = require('../../controller/ApiAuthorization')
 const { requireLogin } = require('../../middleware/login_middleware')
 const requireFunctionPermission = require('../../middleware/function_middleware')
 const routerAuth = express.Router()
 
 const initAuthorization = (app) => {
+    //Add feature
     routerAuth.post('/groups/add', addGroup)
     routerAuth.post('/modules/add', addModule)
     routerAuth.post('/functions/add', addFunction)
-    routerAuth.get('/modules/findAll',getAllModules)
-    
-    app.use('/api/v1/',routerAuth)
+    routerAuth.get('/modules/findAll', getAllModules)
+
+    routerAuth.get('/users/findAll', getAllUser)
+    routerAuth.get('/groups/findAll', getAllGroup)
+    routerAuth.get('/groups/findGroup',getGroupByUserId)
+    routerAuth.get('/functions/findFunction',getFunctionByUserId)
+    routerAuth.get('/functions/findAll',getAllFunction)
+    routerAuth.get('/modules/getGroup',)
+    app.use('/api/v1/', routerAuth)
 }
 
 module.exports = initAuthorization
